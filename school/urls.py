8c7ce1e54fefe495a7c3c74ca45945a67618f8cf
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-01-27 19:39:26 +0200

    Add sign-in page

--- school/urls.py
+++ school/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import view_index
+from .views import view_index, view_sign_in
 
 
 app_name = 'school'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', view_index, name='index')
+    path('', view_index, name='index'),
+    path('sign-in/', view_sign_in, name='sign-in')
 ]
--- school/views.py
+++ school/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def view_index(request):
     return render(request, 'school/index.html')
+
+def view_sign_in(request):
+    return render(request, 'school/sign-in.html')
